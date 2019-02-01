3662de55db37ee25e1abb65089c52bda019afc22
isipython <46680362+isipython@users.noreply.github.com>
2019-02-01 10:32:33 -0800

    Update run.py

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ items.add(Item('orange', 2))
 items.add(Item('bannana', 2))
 items.add(Item('pieapple', 2))
 items.add(Item('milk', 2))
-
+print("salam")
 print("Here is your list: ")
 for item in items:
     print(item)
